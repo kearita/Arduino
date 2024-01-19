60d8e272434409a13c21a0ad9d1c15305771d21d
Keara Taylor Lopez <96399549+kearita@users.noreply.github.com>
2024-01-19 14:23:55 -0500

    Add files via upload
    Final commit -- calibrated sensor readings

--- soil_moisture_sensor.ino
+++ soil_moisture_sensor.ino
@@ -1,18 +1,18 @@
+// most sites approximate an ideal humidity level to lie between 40%-60%, but this varies on many factors
+// including outside temperature, and the type/age of the plant
+
 #include <LiquidCrystal.h>
 
-const int dry = 700; // represents MIN soil moisture level (needs to be calibrated)
-const int wet = 500; // represents MAX soil moisture level (needs to be calibrated)
+const int dry = 650; // represents MIN dry soil moisture level
+const int wet = 500; // represents MAX wet soil moisture level
 LiquidCrystal lcd(12,11,5,4,3,2);
-
-// < 500 too wet
-  // 500 - 750 good
-  // > 750 dry -- needs to be watered
 
 void setup() {
   lcd.begin(16,2);
   Serial.begin(9600);
 }
 
+// this function is for the purpose of displaying the soil moisture % on LCD screen for quicker viewing
 int getPercent(int value) {
   int percentage = map(value, wet, dry, 100, 0);
   return percentage;
@@ -23,6 +23,9 @@ void loop() {
   int moisturePercent = getPercent(moistureLevel);
   Serial.print("Analog output: ");
   Serial.println(moistureLevel);
+  Serial.print("Percent humiditiy: ");
+  Serial.print(moisturePercent);
+  Serial.println("%");
   
   if (moistureLevel < wet) {
     Serial.println("Status: soil is too wet");
@@ -34,12 +37,9 @@ void loop() {
     Serial.println("Status: soil is too dry -- need to water");
   }
   lcd.setCursor(0,0);
-    lcd.print("Level: ");
-    lcd.setCursor(0, 1);
-    lcd.print(moistureLevel);
-    lcd.setCursor(0, 1);
-    lcd.print("Moisture %: ");
-    lcd.println(moisturePercent);
+  lcd.print("Humidity: ");
+  lcd.print(moisturePercent);
+  lcd.print("%");
   delay(500);
   Serial.println();
 }
